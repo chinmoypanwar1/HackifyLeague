f38886137bb3ee55d6bd59da18238b2cdcb91227
GreyHarp <chinmoypanwar1@gmail.com>
2024-09-11 23:39:37 +0530

    git merge

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -9,6 +9,6 @@ router.route("/login").post(loginUser)
 
 // secured Routes
 router.route("/logout").post(verifyJWT, logoutUser)
-router.route("/refresh-token").pose(refreshAccessToken)
+router.route("/refresh-token").post(refreshAccessToken)
 
 export default router;
